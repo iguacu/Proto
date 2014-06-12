6fb79e2b6fb134d6b69bc9871eaf29f9c638e90c
iguacu <tbigmedley@gmail.com>
2014-06-13 01:58:38 +0900

    나가기
    구현함

--- Proto/Assets/Pointer.js
+++ Proto/Assets/Pointer.js
@@ -4,8 +4,7 @@ var distance = -3;
 var lift = 1.5;
 
 function Update () {
- transform.position=Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
- Camera.main.ScreenToWorldPoint(Input.mousePosition).y,-3);
+ transform.position=Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,-3);
  Vector3(0, lift, distance);
 // transform.LookAt(target);
 }
---
+++ Proto/Assets/returnOutside.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returnOutside : MonoBehaviour {
+	
+		
+	void OnMouseDown()
+	{
+		Application.LoadLevel ("Hotel");
+	}
+}
